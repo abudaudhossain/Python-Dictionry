763364aeda14ccb1b1958913a65fd58797bd079e
AbuDaud <abudaudhossainsumon@gmail.com>
2021-02-06 18:22:25 +0600

    use some bunlid function

--- pyDict.py
+++ pyDict.py
@@ -2,8 +2,12 @@ thisdict = {
     "brand": "Ford",
     "model": "Mustang",
     "year": 1943,
-    "year": 2021
+    "year": 2021,
+    "electric": False,
+    "color": ["red", "White", "blue"]
 }
 
 print(thisdict)
 print(thisdict["brand"])
+print(len(thisdict))
+print(type(thisdict))
